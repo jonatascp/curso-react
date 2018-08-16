58776750fc10c202ebaec3319261013236bdb905
Jonatas Pereira <jonatascp@gmail.com>
2018-08-16 09:08:56 -0300

    create react util

--- exercicios_react/ex.6/index.jsx
+++ exercicios_react/ex.6/index.jsx
@@ -6,8 +6,6 @@ import Member from './member'
 ReactDOM.render(
     <Family lastName='Pereira'>
         <Member name='Pessoa 1' />
-        <Member name='Pessoa 2' />
-        <Member name='Pessoa 3' />
     </Family>
     , document.getElementById('app')
 )
