4753725b6747b6ec3fe81659244b3dc3e4bb16c3
Jonatas Pereira <jonatascp@gmail.com>
2018-08-16 08:46:51 -0300

    first example of component

--- exercicios_react/ex.4/index.jsx
+++ exercicios_react/ex.4/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './family'
+import Component, { Segundo } from './component'
 
 ReactDOM.render(
     <div>
-        <Family />
+        <Component />
+        <Segundo />
     </div>
     , document.getElementById('app')
 )
