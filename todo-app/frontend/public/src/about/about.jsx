410aa9681a1437b187cd09ec8853a67abdd155b7
Jonatas Pereira <jonatascp@gmail.com>
2019-03-26 10:54:49 -0300

    create component GRID

--- todo-app/frontend/public/src/about/about.jsx
+++ todo-app/frontend/public/src/about/about.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>Nossa História</h2>
+        <p>Texto qualquer</p>
+        <h2>Imprensa</h2>
+        <p>Texto qualquer</p>
     </div>
 )
