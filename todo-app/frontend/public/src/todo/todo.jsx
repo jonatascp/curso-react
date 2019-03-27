d96d57d0236431caec369bea2505d0833e3fbe4a
Jonatas Pereira <jonatascp@gmail.com>
2019-03-27 13:56:33 -0300

    create handleAdd and handleChange on input

--- todo-app/frontend/public/src/todo/todo.jsx
+++ todo-app/frontend/public/src/todo/todo.jsx
@@ -4,11 +4,30 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+        console.log(e)
+    }
+    
+    handleAdd() {
+        console.log(this)
+    }
+    
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} 
+                    handleChange={this.handleChange}
+                    description={this.state.description} />
                 <TodoList />
             </div>
         )
