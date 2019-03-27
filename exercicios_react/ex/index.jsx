c75c3b81be923086b03cc8c35085e89efd452e23
Jonatas Pereira <jonatascp@gmail.com>
2019-03-27 16:49:32 -0300

    add redux on field

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -4,10 +4,11 @@ import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import Field from './field'
+import fieldReducer from './filedReducer'
 
 
 const reducers = combineReducers({
-    field: () => ({ value: 'Opa' })
+    field: fieldReducer
 })
 
 ReactDOM.render(
