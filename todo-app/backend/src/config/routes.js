333808ff17d50c7e8e2fe9401f3fb8209c1cd78f
Jonatas Pereira <jonatascp@gmail.com>
2019-04-02 10:07:04 -0300

    aplly redux on todo-app

--- todo-app/backend/src/config/routes.js
+++ todo-app/backend/src/config/routes.js
@@ -2,20 +2,11 @@ const express = require('express')
 
 module.exports = function(server) {
 
-<<<<<<< HEAD
     // API Routes
-=======
-    //API Routes
->>>>>>> ff7171c80f660100bf2fe862fc9e9926ff0cfa39
     const router = express.Router()
     server.use('/api', router)
 
     //TODO Routes
     const todoService = require('../api/todo/todoService')
-<<<<<<< HEAD
     todoService.register(router, '/todos')   
-=======
-    todoService.register(router, '/todos')
-
->>>>>>> ff7171c80f660100bf2fe862fc9e9926ff0cfa39
 }
--- todo-app/frontend/public/src/todo/todoList.jsx
+++ todo-app/frontend/public/src/todo/todoList.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import IconButton from '../template/iconButton'
+import { remove, marksAsDone, marksAsPending } from './todoActions'
 
-export default props => {
+const TodoList = props => {
 
     const renderRows = () => {
         const list = props.list || []
@@ -11,11 +14,11 @@ export default props => {
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
-                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                        onClick={() => props.marksAsDone(todo)}></IconButton>
                     <IconButton style='warning' icon='undo' hide={!todo.done}
-                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                        onClick={() => props.marksAsPending(todo)}></IconButton>
                     <IconButton style='danger' icon='trash-o' hide={!todo.done}
-                        onClick={() => props.handleRemove(todo)}></IconButton>
+                        onClick={() => props.remove(todo)}></IconButton>
                 </td>
             </tr>
         ))
@@ -36,3 +39,9 @@ export default props => {
         </table>
     )
 }
+
+const mapStateToProps = state => ({list: state.todo.list})
+const mapDispatchToProps = dispatch => 
+    bindActionCreators({remove, marksAsDone, marksAsPending}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
